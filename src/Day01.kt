b226b8987beb4775d0403a2788d8202e5aadbaa5
sdboyapati <deepu.unity@gmail.com>
2024-12-02 17:36:36 -0500

    Day 1 both parts completed

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,36 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val (left, right) = input.map { line ->
+            val first = line.substringBefore(" ").toInt()
+            val second = line.substringAfterLast(" ").toInt()
+            first to second
+        }.unzip()
+
+        val result = left.sorted().zip(right.sorted()).sumOf { (a, b) ->
+            abs(a - b)
+        }
+        return result
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        val (left, right) = input.map { line ->
+            val first = line.substringBefore(" ").toLong()
+            val second = line.substringAfterLast(" ").toLong()
+            first to second
+        }.unzip()
+
+        val frequencies: Map<Long, Int> = right.groupingBy { it }.eachCount()
+
+        val result = left.sumOf { num ->
+            frequencies.getOrDefault(num, 0) * num
+        }
+
+        return result
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
